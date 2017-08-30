69c6710285c14f4a5f725fa74a469c7454e50d61
chuchu1313 <fangyu1313@gmail.com>
2017-08-30 18:48:48 +0800

    Insertion sort

---
+++ sort-by-golang/insertion-sort/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	unsortArray = []int{93,20,73,73,36,79,64,62,37,31}
+	fmt.Println("\nFinal sort array:", insertionSort(unsortArray))
+}
+
+func insertionSort(unsortArray []int) []int {
+	for i := 1; i < len(unsortArray); i++ {
+		for j := i; j > 0; j-- {
+			if unsortArray[j] < unsortArray[j-1] {
+				unsortArray[j], unsortArray[j-1] = unsortArray[j-1], unsortArray[j]
+			} else {
+				break
+			}
+		}
+		fmt.Println(unsortArray)
+	}
+	return unsortArray
+}
